1dea8d974cef5fa445d4a7d4f9716c37734a769d
Lord Fugoku <richardokonicha@gmail.com>
2023-04-08 03:17:16 +0000

    config clerk auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,43 @@
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  SignIn
+} from "@clerk/nextjs";
+import { useRouter } from "next/router";
+
 
 import "~/styles/globals.css";
 
+// const publicPages = ["/sign-in/[[...index]]", "/sign-up/[[...index]]"];
+const publicPages = [""]
+
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+  const isPublicPage = publicPages.includes(pathname);
+
+  return (
+    <ClerkProvider {...pageProps}>
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            {/* <RedirectToSignIn /> */}
+            <SignIn/>
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
